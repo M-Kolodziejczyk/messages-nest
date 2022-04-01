bb9a01f6447a4c2093b7a2ee677cc091b706173e
Michal <kolodziejczykmichal269@gmail.com>
2022-04-01 20:12:37 +0200

    create controller

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,17 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+
+@Controller('messages')
+export class MessagesController {
+  @Get()
+  listMessgaes() {}
+
+  @Post()
+  createMessage(@Body() body: any) {
+    console.log('BODY', body);
+  }
+
+  @Get('/:id')
+  getMessage(@Param('id') id: string) {
+    console.log(id);
+  }
+}
